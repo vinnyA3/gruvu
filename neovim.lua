4eb5bcc9cd105c5cbc2e5f8098d6a9fe9e093d68
ankur311sudo <ankur311275@gmail.com>
2026-01-01 23:49:53 +0530

    updates

--- neovim.lua
+++ neovim.lua
@@ -1,9 +1,16 @@
 return {
-	{ "ellisonleao/gruvbox.nvim" },
+	{
+		"sainnhe/gruvbox-material",
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_transparent_background = 1
+		end,
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "gruvbox-material",
 		},
 	},
 }
